f6ebf53ff3671dd438db65b801b1131c845f1bf9
ylambda <spy.lambda@gmail.com>
2014-08-19 15:13:53 -0400

    dont use the compass sprite name with hash

--- config.rb
+++ config.rb
@@ -1,6 +1,5 @@
-# Require any additional compass plugins here.
-
 # Set this to the root of your project when deployed:
+
 http_path = "/"
 css_dir = "stylesheets"
 sass_dir = "sass"
@@ -8,18 +7,11 @@ images_dir = "sprites"
 javascripts_dir = "javascripts"
 generated_images_dir = "images"
 
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
+# disable debugging comments
 line_comments = false
 
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+# dont use the compass hash for filenames
+on_sprite_saved do |filename|
+    target = filename.sub(/-s[a-f0-9]{10}\.png/, ".png")
+    FileUtils.mv(filename, target)
+end
